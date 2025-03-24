b03bb7426d387f2633b559d5a998883a69a5129e
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-03-24 23:07:46 +0530

    Update dashboard.php

--- Richwin Dashboard/dashboard.php
+++ Richwin Dashboard/dashboard.php
@@ -72,3 +72,24 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xl-3 col-md-6">
+                     <div class="card bg-success text-white mb-4">
+                         <div class="card-body">Feedback View</div>
+                        <div class="card-footer d-flex align-items-center justify-content-between">
+                             <a class="small text-white stretched-link" href="FeedBack_Table.php">View Details</a>
+                            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                         </div>
+                      </div>
+                 </div>
+
+
+                 <div class="col-xl-3 col-md-6">
+                     <div class="card bg-danger text-white mb-4">
+                        <div class="card-body">LogOut</div>
+                         <div class="card-footer d-flex align-items-center justify-content-between">
+                             <a class="small text-white stretched-link" href="login.php">View Details</a>
+                            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                         </div>
+                     </div>
+                 </div>
+            </div>
