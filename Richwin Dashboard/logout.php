1c485c9d8d41b3766908dd7ddff96dc8dac39754
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-03-25 21:31:43 +0530

    Update logout.php

--- Richwin Dashboard/logout.php
+++ Richwin Dashboard/logout.php
@@ -20,3 +20,34 @@
     <!--Stylesheet-->
     <link rel="stylesheet" href="css/style1.css">
 </head>
+<body>
+    
+    <div class="side-bar">
+        <header>
+        <h1>Richwin Admin Panel</h1>
+    </header>
+        <div class="menu">
+            <div class="item"><a href="dashboard.php"><i class="fas fa-tachometer-alt"></i>Dashboard</a></div>
+            <div class="item"><a href="room.php"><i class="fa-solid fa-bed"></i>Room </a></div>
+            <div class="item"><a href="login.php"><i class="fa-solid fa-right-from-bracket"></i>Log Out</a></div>
+        </div>
+
+
+
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
+
+
+
+    <!--Java Script-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!--Swiper from Script-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</body>
+</html>
