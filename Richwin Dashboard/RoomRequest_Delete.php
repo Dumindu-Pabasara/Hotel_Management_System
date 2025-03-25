91b715a49e6fba552153c7bb3e15f58ad377e288
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-03-25 21:34:22 +0530

    Update RoomRequest_Delete.php

--- Richwin Dashboard/RoomRequest_Delete.php
+++ Richwin Dashboard/RoomRequest_Delete.php
@@ -42,3 +42,37 @@ $result=mysqli_query($con,$query);
 
 
 }
+.btn:hover{
+    background:darkred;
+    color:#fff;
+}
+</style>
+
+</head>
+<body>
+   <!--Side bar-->
+    <div class="side-bar"style="background:#1d1717;">
+        <header>
+        <h1>Richwin Admin Panel</h1>
+    </header>
+        <div class="menu">
+            <div class="item"><a href="dashboard.php"><i class="fas fa-tachometer-alt"></i>Dashboard</a></div>
+            <div class="item"><a href="RoomRequest_Table.php"><i class="fa-solid fa-bed"></i>Room</a> </div>
+            <div class="item"><a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i>Log Out</a></div>
+        </div>
+</div>
+
+
+       <!--Heder name-->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                   
+                    <div class="card border-1 shadow-sm mb-4"style="margin-left: 20px;">
+                        <div class="card-body">
+                        <h3 class="mb-2"style="color:#1d1717;">Customer Room Request Delete Table</h3>
+                        </div>
+                </div>
+                </div>
+            </div>
+        </div>
