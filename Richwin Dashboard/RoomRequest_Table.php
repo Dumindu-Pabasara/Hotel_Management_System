695347f46172a8d9583b09710bfff69d2171a14f
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-03-27 22:23:10 +0530

    Update RoomRequest_Table.php

--- Richwin Dashboard/RoomRequest_Table.php
+++ Richwin Dashboard/RoomRequest_Table.php
@@ -87,13 +87,7 @@ $result=mysqli_query($con,$query);
                 <?php
                 }
                 ?>
-                
-
-            
-            
-
             </thead>
-       
     </table>
 </div>
 <!--Java Script-->
@@ -103,6 +97,5 @@ $result=mysqli_query($con,$query);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!--Swiper from Script-->
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
 </body>
 </html>
